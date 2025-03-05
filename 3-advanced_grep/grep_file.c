4192f3e902038c596aeed86c998b1fc404d9a9e0
ELF32bit <101274435+ELF32bit@users.noreply.github.com>
2025-03-05 11:20:45 +0300

    Update

---
+++ 3-advanced_grep/grep_file.c
@@ -0,0 +1,41 @@
+#include "grep.h"
+
+#include <stdlib.h> // EXIT_SUCCESS, EXIT_FAILURE, free()
+#include <stdio.h> // fopen(), getline(), printf(), fclose()
+
+GrepFileResult grep_file(const char* file_name, const GrepOptions* options) {
+	/* This time also returning how many matches occured */
+	GrepFileResult grep_file_result;
+	grep_file_result.match_count = 0;
+	grep_file_result.exit_code = EXIT_SUCCESS;
+
+	FILE *file = fopen(file_name, "r");
+	if (file == NULL) {
+		grep_file_result.exit_code = EXIT_FAILURE; // setting error code
+		return grep_file_result;
+	}
+
+	/* Reading file line by line using getline() from stdio.h */
+	char* line = NULL;
+	size_t line_size = 0; // size of the line buffer
+	ssize_t line_length; //extra s means signed, size_t is unsigned by default
+	size_t line_number = 0;
+
+	while ((line_length = getline(&line, &line_size, file)) != -1) {
+		line_number += 1;
+		GrepStringResult grep_string_result = grep_string(line, options);
+		grep_file_result.match_count += grep_string_result.match_count;
+		if (grep_string_result.colored_string != NULL) {
+			printf("%s", ANSI_COLOR_GREEN);
+			printf("%zu", line_number);
+			printf("%s:%s", ANSI_COLOR_CYAN, ANSI_COLOR_RESET);
+			printf("%s", grep_string_result.colored_string);
+			free(grep_string_result.colored_string); // free() after use
+		}
+	}
+
+	if (line != NULL) { free(line); }
+	fclose(file);
+
+	return grep_file_result;
+}
--- 3-advanced_grep/grep_string.c
+++ 3-advanced_grep/grep_string.c
@@ -4,7 +4,7 @@
 #include <string.h> // strlen(), strdup()
 #include <wchar.h> // wcslen(), wcsdup()
 #include <wctype.h> // towupper(), iswalpha()
-#include <stdio.h> // asprintf(), fopen(), getline(), printf(), fclose()
+#include <stdio.h> // asprintf()
 
 /* asprintf() macro for repeated usage without memory leaks */
 /* First use of this macro requires a heap allocated string */
@@ -132,41 +132,4 @@ GrepStringResult grep_string(const char* string, const GrepOptions* options) {
 	grep_string_result.colored_string = colored_string;
 	grep_string_result.match_count = match_count;
 	return grep_string_result;
-}
-
-GrepFileResult grep_file(const char* file_name, const GrepOptions* options) {
-	/* This time also returning how many matches occured */
-	GrepFileResult grep_file_result;
-	grep_file_result.match_count = 0;
-	grep_file_result.exit_code = EXIT_SUCCESS;
-
-	FILE *file = fopen(file_name, "r");
-	if (file == NULL) {
-		grep_file_result.exit_code = EXIT_FAILURE; // setting error code
-		return grep_file_result;
-	}
-
-	/* Reading file line by line using getline() from stdio.h */
-	char* line = NULL;
-	size_t line_size = 0; // size of the line buffer
-	ssize_t line_length; //extra s means signed, size_t is unsigned by default
-	size_t line_number = 0;
-
-	while ((line_length = getline(&line, &line_size, file)) != -1) {
-		line_number += 1;
-		GrepStringResult grep_string_result = grep_string(line, options);
-		grep_file_result.match_count += grep_string_result.match_count;
-		if (grep_string_result.colored_string != NULL) {
-			printf("%s", ANSI_COLOR_GREEN);
-			printf("%zu", line_number);
-			printf("%s:%s", ANSI_COLOR_CYAN, ANSI_COLOR_RESET);
-			printf("%s", grep_string_result.colored_string);
-			free(grep_string_result.colored_string); // free() after use
-		}
-	}
-
-	if (line != NULL) { free(line); }
-	fclose(file);
-
-	return grep_file_result;
 }
